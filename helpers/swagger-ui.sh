5d391209033c89d4d7bffc9d8cf12d2879c2ab29
idasilva <isaias.carvalho986@gmail.com>
2020-12-06 02:37:39 -0300

    improve process

--- helpers/swagger-ui.sh
+++ helpers/swagger-ui.sh
@@ -2,7 +2,9 @@
 
 VERSION=$(<./VERSION)
 ROOT_DIR=$(pwd)
-echo "version: $VERSION"
+FILENAME="index.html"
+SEARCH="https://petstore.swagger.io/v2/swagger.json"
+REPLACE="https://digital-swagger.s3.amazonaws.com/api.yml"
 
 
 if mkdir -p $ROOT_DIR/helpers/dist; then
@@ -22,9 +24,19 @@ else
   exit 1
 fi
 
-
 if tar -vzxf $ROOT_DIR/helpers/dist/$VERSION.tar.gz; then
      echo "successfully unzipped" >&2
 else
   echo "error when unzipping file"
+fi
+
+cd $ROOT_DIR/helpers/dist/swagger-ui-3.24.3/dist/
+
+echo "version: $SEARCH"
+echo "version: $REPLACE"
+
+
+if [[ $SEARCH != "" && $REPLACE != "" ]]; then
+  sed -i -e "s|$SEARCH|$REPLACE|g" $ROOT_DIR/helpers/dist/swagger-ui-3.24.3/dist/$FILENAME
+
 fi
